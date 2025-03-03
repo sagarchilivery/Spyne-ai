0c7219146d19c650a8617f47cc1db380a760901f
Sagar <chiliverysagar@gmail.com>
2025-03-03 14:43:29 +0530

    fix: update port mapping in Dockerfile and docker-compose.yml from 1337 to 8080

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . .
 
 RUN npm install
 
-EXPOSE 1337
+EXPOSE 8080
 
 CMD ["npm", "run" ,"start"]
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(
